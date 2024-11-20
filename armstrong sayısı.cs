17540aafbb7b16153fbc006ee021db48fde7a626
senasafa <senasafa8@gmail.com>
2024-11-20 23:08:40 +0300

    Add files via upload

---
+++ armstrong sayısı.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir sayı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+       
+        int basamakSayisi = sayi.ToString().Length;
+
+        int toplam = 0;
+        int orijinalSayi = sayi;
+
+        
+        while (sayi > 0)
+        {
+            int basamak = sayi % 10; //
+            toplam += (int)Math.Pow(basamak, basamakSayisi); 
+            sayi /= 10;
+        }
+
+       
+        if (toplam == orijinalSayi)
+        {
+            Console.WriteLine("{orijinalSayi} bir Armstrong sayısıdır.");
+        }
+        else
+        {
+            Console.WriteLine("{orijinalSayi} bir Armstrong sayısı değildir.");
+        }
+    }
+}
+
